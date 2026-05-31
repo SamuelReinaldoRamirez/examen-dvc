8bb6419176a453a9ae4d31f166386c6b5821e978
Samuel <samuel.reinaldo.ramirez@gmail.com>
2026-05-31 05:07:58 +0000

    update split with y_binned + new analysis

--- src/data/split.py
+++ src/data/split.py
@@ -4,18 +4,24 @@ import os
 
 df = pd.read_csv("data/raw_data/raw.csv")
 
-y = df["silica_concentrate"]
 X = df.drop(columns=["silica_concentrate"])
+y = df["silica_concentrate"]
+
+y_binned = pd.qcut(y, q=10, duplicates="drop")
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y_binned
 )
 
-os.makedirs("data/processed", exist_ok=True)
+os.makedirs("data/processed_data", exist_ok=True)
 
 X_train.to_csv("data/processed_data/X_train.csv", index=False)
 X_test.to_csv("data/processed_data/X_test.csv", index=False)
 y_train.to_csv("data/processed_data/y_train.csv", index=False)
 y_test.to_csv("data/processed_data/y_test.csv", index=False)
 
-print("Split done")
+print("Split stratifié avec y_binned terminé")
